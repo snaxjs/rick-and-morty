10b55e60472b00ac59a271d0ca475d097d2449a5
snaxd <nwpress@yandex.ru>
2023-08-18 22:32:01 +0700

    Добавил storybook

--- src/components/CharCard/CharCard.stories.tsx
+++ src/components/CharCard/CharCard.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 
-import CharCard from "./CharCard";
+import CharCard from "./index";
 
 const meta: Meta<typeof CharCard> = {
   component: CharCard,
