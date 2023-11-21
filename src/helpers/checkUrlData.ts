02f0a9968eedeb69090942cd975e550927f1698c
snaxd <nwpress@yandex.ru>
2023-11-22 03:03:07 +0700

    Добавил хелпер проверки данных по url

---
+++ src/helpers/checkUrlData.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { ErrorHandler } from "../utils/ErrorHandler";
+
+export const checkUrlData = async (url: string): Promise<boolean> => {
+  let result = true;
+  await axios.get(url).catch((error) => {
+    ErrorHandler(error);
+    result = false;
+  });
+  return result;
+};
--- src/utils/ErrorHandler.ts
+++ src/utils/ErrorHandler.ts
@@ -1,7 +1,7 @@
-export function ErrorHandler(e: Error): null {
+export function ErrorHandler(e: Error) {
   console.groupCollapsed("ERROR");
   console.error("ERROR NAME", e.name);
   console.error("ERROR MESSAGE: ", e.message);
   console.error("ERROR STACK: ", e.stack);
-  return null;
+  return e;
 }
