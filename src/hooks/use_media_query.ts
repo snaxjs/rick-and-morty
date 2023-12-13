7f6fccfe6142813b32aee3c0646f6a08997b6958
snaxd <nwpress@yandex.ru>
2023-12-13 15:33:03 +0700

    use_media_query -> useMediaQuery

--- src/hooks/use_media_query.ts
+++ src/hooks/use_media_query.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export function use_media_query(query: string): boolean {
+export function useMediaQuery(query: string): boolean {
   const getMatches = (query: string): boolean => {
     if (typeof window !== "undefined") {
       return window.matchMedia(query).matches;
